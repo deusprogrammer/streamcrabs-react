86f783b4d42c4a922e30732d4f58b7d2c0561706
Michael Main <deusprogrammer@gmail.com>
2021-12-14 12:34:12 -0600

    Updated home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 export default class Home extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Streamcrabs</h1>
+            <div style={{textAlign: "center"}}>
+                <img style={{width: "500px"}} src={`${PUBLIC_URL}/streamcrab.png`} />
+                <h1>Welcome to Streamcrabs</h1>
                 <h2>What is Streamcrabs?</h2>
                 <p>Streamcrabs is an open source Twitch bot and tool for setting up custom alerts and tools.</p>
                 <h2>What does Streamcrabs Currently Support?</h2>
