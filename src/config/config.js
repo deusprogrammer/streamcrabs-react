5963bb94b1d447fa3ec2cdf1a71883966ffe7ad2
Michael Main <deusprogrammer@gmail.com>
2022-01-07 00:27:37 -0600

    Trying injection of environemtn variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 console.log("CLIENT_ID: " + process.env.REACT_APP_TWITCH_CLIENT_ID);
+console.log("API URL:   " + process.env.REACT_APP_API_DOMAIN);
 
 export default {
     //BASE_URL: "http://localhost:8080"
